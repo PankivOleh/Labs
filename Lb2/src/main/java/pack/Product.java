3669fdf01d2e2d12d6960c7861a4e90c3f2fa5f3
PankivOleh <oleg2812poa2006@gmail.com>
2025-09-25 19:48:47 +0300

    Add files via upload

---
+++ Lb2/src/main/java/pack/Product.java
@@ -0,0 +1,34 @@
+package pack;
+
+public class Product {
+    int id;
+    String name;
+    String vurobnuk;
+    int zina , termin , kilkist;
+    public Product( int id , String name , String vurobnuk , int zina , int termin , int kilkist){
+        this.id = id;
+        this.name = name;
+        this.vurobnuk = vurobnuk;
+        this.zina = zina;
+        this.termin = termin;
+        this.kilkist = kilkist;
+    }
+    public void setValue(int zina){
+        this.zina = zina;
+    }
+    public int getValue(){
+        return this.zina;
+    }
+    public int getTermin(){return this.termin;}
+    public String getName(){return this.name;}
+    public String toString(){
+        return "\nПродукт{" +"\n"+
+                "id=" + id +"\n"+
+                "Назва='" + name + '\n' +
+                "Виробник='" + vurobnuk + '\n' +
+                "Ціна=" + zina +"\n"+
+                "Термін придатності=" + termin +"м \n"+
+                "кількість=" + kilkist +"\n"+
+                '}';
+    };
+}
